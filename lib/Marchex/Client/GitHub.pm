90b750a8139502f94096456a5f576ddcd0e87639
Chris Nandor <cnandor@marchex.com>
2016-07-21 15:13:57 -0700

    if available, use Marchex::Client::Tiny

--- lib/Marchex/Client/GitHub.pm
+++ lib/Marchex/Client/GitHub.pm
@@ -107,6 +107,12 @@ sub new {
         $self->{base}   //= $self->{uri} . '/api/v3';
     }
 
+    eval {
+        require Marchex::Client::Tiny;
+        $self->{tiny} = Marchex::Client::Tiny->new;
+    };
+    undef $@;
+
     return $self;
 }
 
@@ -302,6 +308,11 @@ sub init {
 
     return(\%opts);
 
+}
+
+sub format_url {
+    my($self, $url) = @_;
+    return $self->{tiny} ? $self->{tiny}->tinify($url) : $url;
 }
 
 1;
